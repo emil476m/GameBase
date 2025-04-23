89b65dc24946bd1eacd447370e9cb28565e521d7
TheDarkSide1992 <Jenscp171@gmail.com>
2025-04-23 10:56:14 +0200

    fixes ocelot issue, now single file

--- Apps/gateway/src/Program.cs
+++ Apps/gateway/src/Program.cs
@@ -6,17 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 //ocelot
 builder.Configuration
     .AddJsonFile("configs/ocelot.global.json", optional: false, reloadOnChange: true);
-builder.Configuration    
-    .AddJsonFile("configs/ocelot.search_engine-service.json", optional: false, reloadOnChange: true); 
-builder.Configuration
-    .AddJsonFile("configs/ocelot.review_handler-service.json", optional: false, reloadOnChange: true);
-builder.Configuration
-    .AddJsonFile("configs/ocelot.create_service.json", optional: false, reloadOnChange: true);
     //Further services that should be added to gateway added here
     //builder.Configuration.AddJsonFile("configs/ocelot.<NameOfFile>.json", optional: false, reloadOnChange: true); 
 
 builder.Services.AddOcelot(builder.Configuration);
-
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -31,8 +24,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseOcelot().Wait();
-
-app.UseHttpsRedirection();
 
 app.Run();
 
