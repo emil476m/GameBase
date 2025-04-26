185bdfcde02a02cc34f5b3824ca2e0415bc2118a
TheDarkSide1992 <Jenscp171@gmail.com>
2025-04-26 14:37:32 +0200

    fixed potential infinate loop (added limiter)

--- Apps/CreateService/service/Implementations/CreateService.cs
+++ Apps/CreateService/service/Implementations/CreateService.cs
@@ -21,13 +21,17 @@ public class CreateService : IService
 
     private async Task<Guid> CreateGuid()
     {
-        while (true)
+        int limiter = 0;
+        while (limiter < 5)
         {
+            limiter++;
             var guid = Guid.NewGuid();
             if (!await _repository.CheckIfGuidExists(guid))
             {
                 return guid;
             }
         }
+        
+        throw new Exception("Error Occurred try again latter");
     }
 }
