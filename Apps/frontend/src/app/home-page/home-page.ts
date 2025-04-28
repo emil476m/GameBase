d477c757b269949b66d8ad51828101b4ff57f20b
andreas14101 <andreasshansen@live.dk>
2025-04-28 19:06:37 +0200

    home page works

--- Apps/frontend/src/app/home-page/home-page.ts
+++ Apps/frontend/src/app/home-page/home-page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import {ModalController} from "@ionic/angular";
 import {CreateGamePage} from "../CreateGamePage/createGamePage";
+import {GameDto} from "../Models/GameDto";
+import {SearchDto} from "../Models/SearchDto";
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home-page',
@@ -10,8 +14,47 @@ import {CreateGamePage} from "../CreateGamePage/createGamePage";
 })
 export class HomePage {
 
-  constructor(private mc: ModalController) {}
+  gamelist: Array<GameDto> = [];
 
+  constructor(private mc: ModalController, private http: HttpClient) {
+    this.getgameList();
+  }
+
+
+  getgameList()
+  {
+    let page = 0
+    if (this.gamelist.length > 0)
+    {
+      page == this.gamelist.length/10+1;
+    }
+    if(page == 0)
+    {
+      page = 1
+    }
+        try {
+          this.gamelist = [];
+          const call = this.http.get<GameDto[]>(environment.baseURL + "Games/" + 1);
+          call.subscribe((resData: GameDto[]) => {
+            if(page = 1)
+            {
+              this.gamelist = resData;
+            }
+            else
+            {
+              this.gamelist.concat(resData);
+            }
+            console.log(this.gamelist)
+          })
+        } catch (error) {
+          // @ts-ignore
+          if (error.status === 404) {
+            console.log("could not find response for games");
+          } else {
+            console.error("Unexpected error " + error);
+          }
+        }
+  }
 
   openCreateGamePage() {
     this.mc.create({
@@ -21,5 +64,10 @@ export class HomePage {
     ).then(res => {
       res.present();
     });
+  }
+
+  openDetailedGamePage(gameId: string)
+  {
+    window.location.href = "/game/" + gameId;
   }
 }
